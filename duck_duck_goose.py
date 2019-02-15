a4581c8f062071b2e59e241c660da78e27f2b41a
albertomartinezcaballero <46898790+albertomartinezcaballero@users.noreply.github.com>
2019-02-15 13:09:35 +0100

    Add files via upload

--- duck_duck_goose.py
+++ duck_duck_goose.py
@@ -1,37 +1,9 @@
-class producto():
-    def __init__(self, nombre, precio):
-        self.nombre = nombre
-        self.precio = precio
-    def get_nombre(self):
-        return self.nombre
-    def get_precio(self):
-        return self.precio
-class medicamento(producto):
-    def __init__(self,nombre,precio,sustancia,porcentaje):
-        self.sustancia = sustancia
-        self.porcentaje = porcentaje
-
-    def get_sustancia(self):
-        return self.sustancia
-
-    def get_porcentaje(self):
-        return self.porcentaje
-producto1 = producto(nombre="gasas",precio =21)
-producto2 = medicamento(nombre = "aspirina",precio =2,sustancia = "ácido clavulánico", porcentaje = 0)
-producto3 = medicamento(nombre = "paracetamol",precio =45,sustancia = "opio", porcentaje = 30)
-producto4 = medicamento(nombre = "dalsy",precio = 30,sustancia = "cítrico", porcentaje = 40)
-class laboratorio():
-    def __init__(self,nombre,inventario):
-        self.nombre = nombre
-        self.inventario = inventario
-        self.producto1 = producto1
-        self.producto2=  producto2
-        self.producto3 = producto3
-        self.producto4 = producto4
-    def get_nombre(self):
-        return self.get_nombre
-    def get_inventario(self):
-        return self.inventario
-lab =("Bayern",)
-
-print("El laboratorio"lab.get_nombre() "tiene")
+def duckduckgoose(numero,lista):
+    for i in range(lista):
+        if i==numero:
+            return lista[i]
+        else:
+            continue
+lista=["carlos","gonzalo","helena","pablo","sara","lucia"]
+jugador = duckduckgoose(4,lista)
+print(jugador)
